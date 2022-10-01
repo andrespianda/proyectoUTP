2351c66f4d8e80579b28443909be12b6941fe956
andre <andres.pianda00@usc.edu.co>
2022-10-01 16:51:25 -0500

    solucion index - el despliegue

--- src/main/java/connection/DBConnection.java
+++ src/main/java/connection/DBConnection.java
@@ -12,32 +12,42 @@ import java.sql.DriverManager;
  * @author andre
  */
 public class DBConnection {
-     Connection connection;
-    
-    static String bd = "the_pet_shop";
+
+    Connection connection;
+
+    /* static String bd = "the_pet_shop";
     static String port = "3306";
     static String login = "root";
-    static String password = "admin";
+    static String password = "admin";*/
+    
+    static String bd = "railway";
+    static String ip = "containers-us-west-88.railway.app" ;
+    static String password = "vvGYWp3c4MAwPqKMUv23";
+    static String port = "7198";
+    static String login = "root";
+
+    /*MYSQL_URL=mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}*/
 
     public DBConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:" + this.port + "/" + this.bd;
-            connection =  DriverManager.getConnection(url, this.login, this.password);
+            //String url = "jdbc:mysql://localhost:" + this.port + "/" + this.bd;
+            String url = "jdbc:mysql://"+ DBConnection.ip +":" + DBConnection.port + "/" + DBConnection.bd;
+            //connection = DriverManager.getConnection(url, this.login, this.password);
+            connection = DriverManager.getConnection(url, DBConnection.login, DBConnection.password);
             System.out.println("Listo, conectado!!");
         } catch (Exception e) {
             System.out.println("La cagaste en algo, revisa!!");
         }
     }
-    
-    public Connection getConnection(){
-        
+
+    public Connection getConnection() {
+
         return connection;
     }
-    
-    public void  desconectar(){
+
+    public void desconectar() {
         connection = null;
     }
-    
-    
+
 }
